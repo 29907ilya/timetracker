341ffaad6395a962d3f14a726b0c6ebd0bce4fa5
29907ilya <ilya.n.yarosh@gmail.com>
2023-04-17 00:47:13 +0300

    Add server

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,38 @@
 <template>
 <div>
-  <h1>Welcome</h1>
+  <div v-if="loading">Loading...</div>
+  <div v-if="!loading && !error">Welcome</div>
+  <div v-if="!loading && error">Something went wrong</div>
 </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  name: "App"
+  name: "App",
+
+  data() {
+    return {
+      loading: true,
+      error: false
+    }
+  },
+
+  methods: {
+    ...mapActions(["initialLoad"])
+  },
+
+  async mounted() {
+    try {
+      await this.initialLoad();
+      this.loading = false;
+
+    } catch (err) {
+      this.loading = false;
+      this.error = true;
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,28 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        user: {
+            id: null,
+            email: "",
+            userName: ""
+        }
+    },
     getters: {},
-    mutations: {},
-    actions: {},
-    modules: {}
+    mutations: {
+        CURRENT_USER_FETCHED(state, user) {
+            state.user.id = user.id;
+            state.user.email = user.email;
+            state.user.userName = user.userName;
+        }
+    },
+    actions: {
+        async initialLoad(context) {
+            if(localStorage.ttrackerjwt) {
+                Vue.axios.defaults.headers.common.Authorization = `Bearer ${localStorage.ttrackerjwt}`
+            }
+            const res = await Vue.axios.get("/api/auth/currentUser")
+            context.commit("CURRENT_USER_FETCHED", res.data.user)
+        }
+    },
 })
